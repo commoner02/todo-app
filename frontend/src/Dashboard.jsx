20e93d5b18208c1c01c8f025c36f5e56c9ae9b3a
Shuvo <77.shuvo.joy@gmail.com>
2025-12-12 23:27:01 +0600

    frontend setup react+tailwind

---
+++ frontend/src/Dashboard.jsx
@@ -0,0 +1,10 @@
+function Dashboard (){
+    return (
+        <div className="flex m-auto justify-center p-2">
+            <h2 className="bg-red-500">Welcome</h2>
+            <p className="m-1-red">this is a welcome paragraph</p>
+        </div>
+    )
+}
+
+export default Dashboard
